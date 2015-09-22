a2decdfcbca5230da32f97363bb1dd3a3b1df1ef
runeras <rr@1q.dk>
2015-09-22 21:52:04 +0200

    Mouse rotation

--- The Best Game Ever/Assets/Movement.cs
+++ The Best Game Ever/Assets/Movement.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 	public class Movement : MonoBehaviour {
 
 	public float speed;
-	public float runspeed = 12f;
+	public float runspeed = 20f;
 	public float walkspeed = 4f;
 	public float jumpSpeed;
+	public float angle = 0f;
+	public float turnspeed = 1f;
 	public float gravity;
 	private Vector3 moveDirection = Vector3.zero;
+	public Vector3 acspeed;
 
 	void Start() {
+
+
 
 		jumpSpeed = 8.0f;
 		gravity = 20.0f;
@@ -28,8 +33,14 @@ using UnityEngine;
 			speed = walkspeed;
 		}
 
+
+
 		CharacterController controller = GetComponent<CharacterController>();
+		acspeed = controller.velocity;
+		//Debug.Log (acspeed);
+
 		if (controller.isGrounded) {
+
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical"));
 			moveDirection = transform.TransformDirection(moveDirection);
 			moveDirection *= speed;
@@ -37,6 +48,8 @@ using UnityEngine;
 				moveDirection.y = jumpSpeed;
 			
 		}
+		angle += Input.GetAxis ("Mouse X");
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 	}
---
+++ The Best Game Ever/Assets/look.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class look : MonoBehaviour {
+	public float yaw;
+	public float acyaw;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		acyaw = transform.rotation.eulerAngles.x;
+		yaw = Input.GetAxis ("Mouse Y");
+		//Debug.Log (yaw);
+		if (acyaw > 90 && acyaw < 180)
+			transform.Rotate( -20, 0, 0);;
+		if (acyaw > 180 && acyaw < 270)
+			transform.Rotate( -yaw, 0, 0);
+		transform.Rotate( -yaw, 0, 0);
+		//if (eulerAngles.x > 90) {
+		
+		//}
+
+		Debug.Log(transform.rotation.eulerAngles.x);
+	}
+}
