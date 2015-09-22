8b8356d5279fc3857c03c9f9d3c801adae5e1d22
runeras <rr@1q.dk>
2015-09-22 18:17:29 +0200

    Test Af Gui

---
+++ The Best Game Ever/Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+	
+	public class Movement : MonoBehaviour {
+
+	public float speed;
+	public float runspeed = 12f;
+	public float walkspeed = 4f;
+	public float jumpSpeed;
+	public float gravity;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Start() {
+
+		jumpSpeed = 8.0f;
+		gravity = 20.0f;
+		GameObject Player = GameObject.Find("Player");
+		Sved Sved = Player.GetComponent<Sved>();
+	}
+	
+
+
+	void Update() {
+		if (GameObject.Find("Player").GetComponent<Sved>().running) {
+			speed = runspeed;
+		} else {
+			speed = walkspeed;
+		}
+
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+			
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
---
+++ The Best Game Ever/Assets/Sved.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sved : MonoBehaviour {
+
+	float sved = 4;
+	float maxsved = 5;
+	public bool running = false;
+
+	Rect StaminaRect;
+	Texture2D Staminatext;
+
+	// Use this for initialization
+	void Start () {
+		StaminaRect = new Rect (Screen.width / 10, Screen.height * 9 / 10, Screen.width / 3, Screen.height / 50);
+		Staminatext = new Texture2D (1, 1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.LeftShift)){
+			running = true;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftShift)){
+			running = false;
+		}
+		if (running) {
+			sved += 0.7f * Time.deltaTime;
+			if (sved > maxsved) {
+				sved = maxsved;
+				running = false;
+			}
+		} else {
+
+			sved -= 0.25f * Time.deltaTime;
+			if (sved < 0) {
+				sved = 0;
+
+			}
+		}
+	}
+	void OnGUI () {
+		float ratio = sved / maxsved;
+		float rectWitdth = ratio * Screen.width / 3;
+		StaminaRect.width = rectWitdth;
+		if (ratio < 0.5) Staminatext.SetPixel (0, 0, Color.green);
+		else if (ratio > 0.9) Staminatext.SetPixel (0, 0, Color.red);
+		else if (ratio > 0.5 && ratio < 0.9) Staminatext.SetPixel (0, 0, Color.yellow);
+		Staminatext.Apply();
+		GUI.DrawTexture (StaminaRect, Staminatext);
+	}
+}
